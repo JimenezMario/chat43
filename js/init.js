0176c556206729cd5d8655b46df25bdb0c29e641
JimenezMario <81141032+JimenezMario@users.noreply.github.com>
2021-03-23 11:59:40 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+     apiKey: "AIzaSyBkDdgQWgMMHvodTW_HizRyZs68-7gi-Mw",
+    authDomain: "chatejem.firebaseapp.com",
+    projectId: "chatejem",
+    storageBucket: "chatejem.appspot.com",
+    messagingSenderId: "917375038968",
+    appId: "1:917375038968:web:b2388661aafd3cc46dd20d",
+    measurementId: "G-KHRCK5WSNC"
+    });
